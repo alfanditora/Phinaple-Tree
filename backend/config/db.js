f35db8dd89a72e3020b8bb9ec7d379eb4193d3de
alfanditora <ditoraisakbar@gmail.com>
2025-01-10 16:29:55 +0700

    Add docker

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432, // Gunakan default port PostgreSQL jika tidak ada di environment
     dialect: 'postgres',
+    logging: false, // Nonaktifkan logging query SQL jika tidak diperlukan
+    dialectOptions: {
+        connectTimeout: 30000, // Timeout koneksi 30 detik
+    },
 });
 
 module.exports = sequelize;
